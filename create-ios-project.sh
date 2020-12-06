ac2db452a20c58b56c2e9c83df93defa8714b10b
Florian Quirin <info@bytemind.de>
2020-12-06 18:40:05 +0100

    cleaned-up iOS build scripts (WIP), requires further rework;

--- create-ios-project.sh
+++ create-ios-project.sh
@@ -19,26 +19,30 @@ cd $APP_NAME
 sleep 2
 echo "#Adding plugins ..."
 cordova plugin add cordova-plugin-device
+cordova plugin add cordova-plugin-screen-orientation
 cordova plugin add cordova-plugin-geolocation
-#cordova plugin add cordova-plugin-inappbrowser
-cordova plugin add cordova-plugin-inappbrowser-wkwebview
-cordova plugin add cordova-plugin-wkwebview-engine
+cordova plugin add cordova-plugin-inappbrowser
+#cordova plugin add cordova-plugin-inappbrowser-wkwebview
+#cordova plugin add cordova-plugin-wkwebview-engine
 cordova plugin add cordova-plugin-tts
 cordova plugin add cordova-plugin-whitelist
 cordova plugin add cordova-universal-links-plugin
 cordova plugin add cordova-plugin-statusbar
-cordova plugin add cordova-plugin-splashscreen
+#cordova plugin add cordova-plugin-splashscreen		#is now part of platform
 #cordova plugin add https://github.com/apache/cordova-plugin-splashscreen.git -- NOTE: use this in case releases are too old
 cordova plugin add cordova-plugin-cache-clear
-cordova plugin add plugin_mods/speechrecognition/org.apache.cordova.speech.speechrecognition
+#TODO: needs complete remake
+#cordova plugin add plugin_mods/speechrecognition/org.apache.cordova.speech.speechrecognition
 #cordova plugin add de.appplant.cordova.plugin.local-notification
 #cordova plugin add plugin_mods/localnotifications/de.appplant.cordova.plugin.local-notification
-cordova plugin add cordova-plugin-local-notification
+#cordova plugin add cordova-plugin-local-notification
+cordova plugin add https://github.com/timkellypa/cordova-plugin-local-notifications
 cordova plugin add cordova-plugin-file
 cordova plugin add cordova-plugin-nativestorage
 cordova plugin add cordova-plugin-audioinput
 cordova plugin add plugin_mods/iosbackgroundaudio/nl.kingsquare.cordova.background-audio
-cordova plugin add https://github.com/EddyVerbruggen/Insomnia-PhoneGap-Plugin.git
+#cordova plugin add https://github.com/EddyVerbruggen/Insomnia-PhoneGap-Plugin.git
+cordova plugin add https://github.com/tombolaltd/cordova-plugin-insomnia.git
 cordova plugin add phonegap-plugin-battery-status
 cordova plugin add cordova-plugin-eddystone
 #
@@ -46,8 +50,9 @@ cordova plugin add cordova-plugin-eddystone
 sleep 2
 echo "#Updating plugins ..."
 cp "plugin_mods/universallinks/xcodePreferences.js" "plugins/cordova-universal-links-plugin/hooks/lib/ios/xcodePreferences.js"
-cp "plugin_mods/inappbrowser/ios/CDVInAppBrowser_wkwv.m" "plugins/cordova-plugin-inappbrowser-wkwebview/src/ios/CDVInAppBrowser.m"
-cp "plugin_mods/inappbrowser/ios/CDVInAppBrowser_wkwv.h" "plugins/cordova-plugin-inappbrowser-wkwebview/src/ios/CDVInAppBrowser.h"
+#TODO: Update WkWebView version
+#cp "plugin_mods/inappbrowser/ios/CDVInAppBrowser_wkwv.m" "plugins/cordova-plugin-inappbrowser-wkwebview/src/ios/CDVInAppBrowser.m"
+#cp "plugin_mods/inappbrowser/ios/CDVInAppBrowser_wkwv.h" "plugins/cordova-plugin-inappbrowser-wkwebview/src/ios/CDVInAppBrowser.h"
 cp "plugin_mods/cacheclear/ios/CacheClear.m" "plugins/cordova-plugin-cache-clear/src/ios/CacheClear.m"
 cp "plugin_mods/tts/ios/CDVTTS.m" "plugins/cordova-plugin-tts/src/ios/CDVTTS.m"
 #
@@ -59,7 +64,7 @@ npm install xcode
 # add ios platform
 sleep 2
 echo "#Adding platform ..."
-cordova platform add ios@5.0.1
+cordova platform add ios@6.1.1
 #
 # prepare build
 echo "#Preparing build ..."
